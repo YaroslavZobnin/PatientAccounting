088f617efc5612a399a90ac85264d1d8d837205b
Yaroslav Zobnin <zobnin114@gmail.com>
2026-04-12 19:22:04 +0300

    Database connection and an empty authorization window have been added.

--- PatientAccounting/Authorization.Designer.cs
+++ PatientAccounting/Authorization.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            SuspendLayout();
+            // 
+            // Authorization
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.Authorization;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(700, 400);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Authorization";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Authorization";
+            ResumeLayout(false);
         }
 
         #endregion
---
+++ PatientAccounting/DataBaseProcessing.cs
@@ -0,0 +1,16 @@
+﻿using Npgsql;
+using System.Configuration;
+namespace PatientAccounting
+{
+    internal static class DataBaseProcessing
+    {
+        private static string? connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"]?.ConnectionString;
+        public static NpgsqlConnection GetConnection()
+        {
+            Console.WriteLine($"Строка из конфига: '{connectionString}'");
+            if (string.IsNullOrEmpty(connectionString))
+                throw new Exception("Ошибка подключения базы данных");
+            else return new NpgsqlConnection(connectionString);
+        }
+    }
+}
