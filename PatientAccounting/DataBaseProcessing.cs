21d94469614e110f4ae3f0f4181e0bf41a811969
Yaroslav Zobnin <zobnin114@gmail.com>
2026-04-12 19:45:47 +0300

    extra line deleted

--- PatientAccounting/DataBaseProcessing.cs
+++ PatientAccounting/DataBaseProcessing.cs
@@ -7,7 +7,6 @@ namespace PatientAccounting
         private static string? connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"]?.ConnectionString;
         public static NpgsqlConnection GetConnection()
         {
-            Console.WriteLine($"Строка из конфига: '{connectionString}'");
             if (string.IsNullOrEmpty(connectionString))
                 throw new Exception("Ошибка подключения базы данных");
             else return new NpgsqlConnection(connectionString);
