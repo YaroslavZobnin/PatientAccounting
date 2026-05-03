7758cff239da8160bfc8061c9943fde736127e5e
Yaroslav Zobnin <zobnin114@gmail.com>
2026-05-03 21:49:46 +0300

    Implementation of the additional features interface (without implementation)

--- PatientAccounting/GeneralForm.Designer.cs
+++ PatientAccounting/GeneralForm.Designer.cs
@@ -29,14 +29,21 @@
         private void InitializeComponent()
         {
             ButtonsPanel = new Panel();
+            ExtraMenuPanel = new Panel();
+            FullScreenButton = new Button();
+            BackToAuthorizationButton = new Button();
+            ExtraMenuButton = new Button();
             ExitButton = new Button();
             UserControlsPanel = new Panel();
             ButtonsPanel.SuspendLayout();
+            ExtraMenuPanel.SuspendLayout();
             SuspendLayout();
             // 
             // ButtonsPanel
             // 
             ButtonsPanel.BackColor = Color.Transparent;
+            ButtonsPanel.Controls.Add(ExtraMenuPanel);
+            ButtonsPanel.Controls.Add(ExtraMenuButton);
             ButtonsPanel.Controls.Add(ExitButton);
             ButtonsPanel.Dock = DockStyle.Right;
             ButtonsPanel.Location = new Point(774, 0);
@@ -44,8 +51,60 @@
             ButtonsPanel.Size = new Size(126, 550);
             ButtonsPanel.TabIndex = 1;
             // 
+            // ExtraMenuPanel
+            // 
+            ExtraMenuPanel.Anchor = AnchorStyles.Top;
+            ExtraMenuPanel.Controls.Add(FullScreenButton);
+            ExtraMenuPanel.Controls.Add(BackToAuthorizationButton);
+            ExtraMenuPanel.Enabled = false;
+            ExtraMenuPanel.Location = new Point(6, 54);
+            ExtraMenuPanel.Name = "ExtraMenuPanel";
+            ExtraMenuPanel.Size = new Size(114, 75);
+            ExtraMenuPanel.TabIndex = 2;
+            ExtraMenuPanel.Visible = false;
+            // 
+            // FullScreenButton
+            // 
+            FullScreenButton.Anchor = AnchorStyles.Top;
+            FullScreenButton.FlatStyle = FlatStyle.Popup;
+            FullScreenButton.Font = new Font("Segoe UI", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            FullScreenButton.Location = new Point(0, 36);
+            FullScreenButton.Name = "FullScreenButton";
+            FullScreenButton.Size = new Size(114, 33);
+            FullScreenButton.TabIndex = 1;
+            FullScreenButton.Text = "На весь экран";
+            FullScreenButton.UseVisualStyleBackColor = true;
+            // 
+            // BackToAuthorizationButton
+            // 
+            BackToAuthorizationButton.Anchor = AnchorStyles.Top;
+            BackToAuthorizationButton.FlatStyle = FlatStyle.Popup;
+            BackToAuthorizationButton.Location = new Point(0, 3);
+            BackToAuthorizationButton.Name = "BackToAuthorizationButton";
+            BackToAuthorizationButton.Size = new Size(114, 27);
+            BackToAuthorizationButton.TabIndex = 0;
+            BackToAuthorizationButton.Text = "Авторизация";
+            BackToAuthorizationButton.UseVisualStyleBackColor = true;
+            // 
+            // ExtraMenuButton
+            // 
+            ExtraMenuButton.Anchor = AnchorStyles.Top;
+            ExtraMenuButton.Cursor = Cursors.Hand;
+            ExtraMenuButton.FlatAppearance.BorderColor = Color.Gray;
+            ExtraMenuButton.FlatStyle = FlatStyle.Flat;
+            ExtraMenuButton.Font = new Font("Segoe UI", 7.8F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            ExtraMenuButton.Location = new Point(6, 12);
+            ExtraMenuButton.Name = "ExtraMenuButton";
+            ExtraMenuButton.Size = new Size(114, 27);
+            ExtraMenuButton.TabIndex = 1;
+            ExtraMenuButton.Text = "Дополнительно";
+            ExtraMenuButton.UseVisualStyleBackColor = true;
+            ExtraMenuButton.Click += ExtraMenuButton_Click;
+            // 
             // ExitButton
             // 
+            ExitButton.Anchor = AnchorStyles.Bottom;
+            ExitButton.Cursor = Cursors.Hand;
             ExitButton.FlatStyle = FlatStyle.Popup;
             ExitButton.Location = new Point(14, 511);
             ExitButton.Name = "ExitButton";
@@ -78,6 +137,7 @@
             ShowIcon = false;
             StartPosition = FormStartPosition.CenterScreen;
             ButtonsPanel.ResumeLayout(false);
+            ExtraMenuPanel.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -85,5 +145,9 @@
         private Panel ButtonsPanel;
         private Panel UserControlsPanel;
         private Button ExitButton;
+        private Panel ExtraMenuPanel;
+        private Button ExtraMenuButton;
+        private Button BackToAuthorizationButton;
+        private Button FullScreenButton;
     }
 }
--- PatientAccounting/GeneralForm.cs
+++ PatientAccounting/GeneralForm.cs
@@ -1,24 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace PatientAccounting
+﻿namespace PatientAccounting
 {
     public partial class GeneralForm : Form
     {
         public GeneralForm()
         {
             InitializeComponent();
-            AdditionalButtons.Show(new Point(200, 300));
+
         }
 
         private void ExitButton_Click(object sender, EventArgs e) => this.Close();
 
+        private void ExtraMenuButton_Click(object sender, EventArgs e) => OpenAdditionalButtons();
+
+        private void OpenAdditionalButtons()
+        {
+            SwitchAdditionalMode();
+            SwitchVisualActivityExtraMenuButton();
+            if (ExtraMenuPanel.Visible)
+                ExtraMenuPanel.BringToFront();
+        }
+        private void SwitchAdditionalMode()
+        {
+            ExtraMenuPanel.Enabled = !ExtraMenuPanel.Enabled;
+            ExtraMenuPanel.Visible = !ExtraMenuPanel.Visible;
+        }
+        private void SwitchVisualActivityExtraMenuButton()
+        {
+            if (ExtraMenuPanel.Visible)
+            {
+                ExtraMenuButton.ForeColor = Color.Red;
+                ExtraMenuButton.FlatAppearance.BorderColor = Color.Red;
+            }
+            else
+            {
+                ExtraMenuButton.ForeColor = SystemColors.ControlText;
+                ExtraMenuButton.FlatAppearance.BorderSize = 1;
+                ExtraMenuButton.FlatAppearance.BorderColor = Color.Gray;
+            }
+        }
     }
 }
